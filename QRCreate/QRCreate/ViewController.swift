154c384bce9c3e574ef4940c3958ad15c39a16f8
XIAO <xjydev@163.com>
2017-03-07 18:30:07 +0800

    init

---
+++ QRCreate/QRCreate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRcreate
+//
+//  Created by xiaodev on Mar/7/17.
+//  Copyright © 2017 xiaodev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
